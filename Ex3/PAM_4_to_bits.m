46ab32fe5d49698ddeb18140a49ef130a36f491c
crockent <echatzakis1@isc.tuc.gr>
2024-06-19 10:35:58 +0300

    finished code

---
+++ Ex3/PAM_4_to_bits.m
@@ -0,0 +1,17 @@
+function est_bit = PAM_4_to_bits(X, A,b)
+    levels = [-3 -1 1 3] * A;
+    % Initialize an empty array to hold the estimated bits
+    est_bit = zeros(1,length(b));
+
+ for i = 1:length(X)
+        if X(i) == levels(1)
+            est_bit(2*i-1:2*i) = [0 0];
+        elseif X(i) == levels(2)
+            est_bit(2*i-1:2*i) = [0 1];
+        elseif X(i) == levels(3)
+            est_bit(2*i-1:2*i) = [1 1];
+        else % X(i) == levels(4)
+            est_bit(2*i-1:2*i) = [1 0];
+        end
+    end
+end
---
+++ Ex3/detect_4_PAM.m
@@ -0,0 +1,8 @@
+function est_X = detect_4_PAM(Y, A)
+    levels = [-3 -1 1 3] * A;
+    est_X = zeros(size(Y));
+    for i = 1:length(Y)
+        [~, idx] = min(abs(levels - Y(i)));
+        est_X(i) = levels(idx);
+    end
+end
